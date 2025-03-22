24b3b8ec2c158549d46dfb36145070afcaf3cf4a
Mostak <mostakahmedshahad74@gmail.com>
2025-03-23 01:06:08 +0600

    update

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "6257156"
+    serviceWorkerVersion: "2568074790"
   }
 });
