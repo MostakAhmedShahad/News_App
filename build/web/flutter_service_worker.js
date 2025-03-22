0f36c6944beeba016717e04fdc0e2dc1b0135671
Mostak <mostakahmedshahad74@gmail.com>
2025-03-23 00:39:57 +0600

    updated

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ba12938a6897bef70c368b10dfdaa53a
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "4ca12fad4ebbf65ac31f3ad32da0948b",
+"flutter_bootstrap.js": "531383dc3ef43979d7ca49451b0d6d26",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
